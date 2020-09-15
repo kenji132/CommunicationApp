e9aacf54894321728e079d91cc3f7fb46da73578
kenji132 <ue52483@gmail.com>
2020-09-15 09:42:30 +0900

    firebaseを用いたユーザー登録機能追加

---
+++ app/src/main/java/com/example/communication_app/LoginActivity.kt
@@ -0,0 +1,31 @@
+package com.example.communication_app
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        login_button_login.setOnClickListener {
+            val email = email_edittext_login.text.toString()
+            val password = password_edittext_login.text.toString()
+
+            Log.d("Login", "Attempt login with email/pw: $email/***")
+
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+//                .addOnCompleteListener()
+//                .add
+        }
+
+        back_to_registration_textview.setOnClickListener {
+            finish()
+        }
+
+    }
+}
